5a9bda961ac41ceee31339e2558077a5965c1882
MateusRodr <mr5508113@gmail.com>
2025-11-01 23:37:12 -0300

    add mappers

---
+++ src/mappers/user.mapper.ts
@@ -0,0 +1,22 @@
+import { User } from "../entity/user.entity";
+import { User as PrismaUser } from "@prisma/client"
+
+export class UserMapper {
+    static toDomain(raw: PrismaUser): User {
+        return new User({
+            id: raw.id,
+            email: raw.email,
+            name: raw.name,
+            password: raw.password
+        });
+    }
+
+    static toPrisma(user: User): PrismaUser {
+        return {
+            id: user.id,
+            email: user.getEmail(),
+            name: user.getName(),
+            password: user.getPassword()
+        };
+    }
+}
